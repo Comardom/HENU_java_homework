ac308f783bdb400aec79a2b72444c3f75b9d42ee
Comardom <Comardom-Xeemen@outlook.com>
2025-04-03 00:55:17 +0800

    大致写完了，但是有些异常处理有问题

--- S5/src/S5_2/S5_2.java
+++ S5/src/S5_2/S5_2.java
@@ -1,5 +1,9 @@
 package S5_2;
 
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class S5_2
@@ -7,11 +11,83 @@ public class S5_2
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
-		String[] tmp = new String[4];
+		full:
 		for(;;)
 		{
-
+			User aUser = new User();
+			String[] tmp = new String[4];
+			register:
+			for(;;)
+			{
+				System.out.println("Now register!");
+				System.out.println("Plz enter your " +
+						"username, password, password(re-enter), phone number" +
+						"sequentially.");
+				for (int i=0;i<tmp.length;i++)
+				{
+					tmp[i] = in.nextLine();
+				}
+				aUser.setUser(tmp[0],tmp[1],tmp[3]);
+				if(SHA256Hash.hash(tmp[2]).equals(aUser.getPasswdHash()))
+				{
+					System.out.println("注册成功");
+					break register;
+				}
+				else
+				{
+					System.out.println("密码不一致，请重新注册");
+				}
+			}
+			signIn:
+			for(;;)
+			{
+				System.out.println("Now sign in!");
+				System.out.println("Plz enter your " +
+						"username, password sequentially, " +
+						"and the password case insensitive.");
+				for (int i=0;i<2;i++)
+				{
+					tmp[i] = in.nextLine();
+				}
+				int i = 0;
+				ifTrue:
+				for(;;)
+				{
+					if(tmp[0].equals(aUser.getUsername())
+							&& SHA256Hash.hash(tmp[1].toLowerCase())
+									.equals(aUser.getPasswdHash()))
+					{
+						Calc calc = new Calc();
+						break full;
+					}
+					else
+					{
+						i++;
+						System.out.println("密码错误，请重新输入！");
+						if(i==2)
+						{
+							System.out.println("登录失败，无法再继续登录");
+							break signIn;
+						}
+					}
+				}
+			}
+			for(;;)
+			{
+				System.out.println("If you wanna register again?");
+				System.out.println("[Y/n]");
+				String input = in.nextLine().trim();
+				if (input.isEmpty() || input.equalsIgnoreCase("y"))
+				{
+					continue full;
+				}
+				else
+				{
+					break full;
+				}
+			}
 		}
+		in.close();
 	}
 }
 class User
@@ -21,14 +97,17 @@ class User
 	private String passwdHash;
 	private String phoneNum;
 
+	public User()
+	{
+		username=originPassword=phoneNum="Preparatory entry";
+	}
 
-	public User(String username, String originPassword,String phoneNum)
+	public void setUser(String username, String originPassword, String phoneNum)
 	{
 		setUsername(username);
 		setOriginPassword(originPassword);
 		setPhoneNum(phoneNum);
 	}
-
 
 	public void setUsername(String username)
 	{
@@ -36,9 +115,8 @@ class User
 	}
 	public void setOriginPassword(String originPassword)
 	{
-		this.originPassword = originPassword;
-		originPassword = null;
-		this.passwdHash = String.valueOf(this.originPassword.hashCode());
+		this.originPassword = originPassword.toLowerCase(Locale.ROOT);
+		this.passwdHash = SHA256Hash.hash(this.originPassword);
 		this.originPassword = null;
 	}
 	public void setPhoneNum(String phoneNum)
@@ -58,5 +136,63 @@ class User
 	public String getPasswdHash()
 	{
 		return passwdHash;
+	}
+}
+//这个类是ai写的，我完全不会
+class Calc
+{
+	public Calc()
+	{
+		String os = System.getProperty("os.name").toLowerCase();
+		String calculatorCmd;
+		if (os.contains("win"))
+		{
+			calculatorCmd = "calc"; // Windows
+		}
+		else if (os.contains("mac"))
+		{
+			calculatorCmd = "open -a Calculator"; // macOS 计算器
+		}
+		else
+		{
+			// 检测 Linux 桌面环境
+			String desktop = System.getenv("XDG_CURRENT_DESKTOP");
+			calculatorCmd = switch (desktop)
+			{
+				case "KDE" -> "kcalc";
+				case "GNOME" -> "gnome-calculator";
+				default -> "xcalc";
+			};
+		}
+		try
+		{
+			new ProcessBuilder(calculatorCmd).start();
+		}
+		catch (IOException e)
+		{
+			System.out.println("无法启动计算器，请检查是否已安装！");
+			e.printStackTrace();
+		}
+	}
+}
+//这个也是ai写的哈哈
+class SHA256Hash
+{
+	public static String hash(String input)
+	{
+		try{
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] hash = digest.digest(input.getBytes());
+			StringBuilder hexString = new StringBuilder();
+			for(byte b : hash)
+			{
+				hexString.append(String.format("%02x", b));
+			}
+			return hexString.toString();
+		}
+		catch(NoSuchAlgorithmException e)
+		{
+			throw new RuntimeException("SHA-256 algorithm not found", e);
+		}
 	}
 }
